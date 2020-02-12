971f59634f5e9accb108f99944a358956c85cef2
Somaia Khalil <sumaya_khair@live.com>
2020-02-12 21:44:17 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,30 @@
-# Enter your procedural solution here!
+
+
+class Multiples
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    # def collect_multiples
+    #     collect_multiples(@limit)
+    # end
+
+    def collect_multiples
+        range = 1...@limit
+        passing_numbers = []
+        range.each do |num|
+            if num % 3 == 0 || num % 5 == 0
+                passing_numbers << num
+            end
+        end
+        passing_numbers
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+end
